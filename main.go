b5d580a47366027e8d235d08b61462d6b30d2aff
OGFris <ilyesou.cherfaoui.18@gmail.com>
2019-10-22 01:37:44 +0200

    A working prototype version.

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/OGFris/MP3ToVideo/mjpeg"
 	"github.com/dhowden/tag"
+	"github.com/tcolgate/mp3"
 	"log"
+	"math/rand"
 	"os"
+	"os/exec"
+	"time"
 )
 
 func main() {
 	var in, out string
 
 	flag.StringVar(&in, "i", "", "give input file")
-	flag.StringVar(&out, "o", "hh", "give output file")
+	flag.StringVar(&out, "o", "", "give output file")
 	flag.Parse()
 
 	if in == "" {
@@ -21,6 +26,13 @@ func main() {
 
 	if out == "" {
 		log.Fatalln("please give a valid output file")
+	}
+
+	if _, err := os.Stat(out); err == nil {
+		err = os.Remove(out)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	if _, err := os.Stat(in); err != nil {
@@ -37,8 +49,49 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Song's Title:", m.Title())
-	fmt.Println("Song's Artist:", m.Artist())
-	fmt.Println("Song's Genre:", m.Genre())
+	rand.Seed(time.Now().UnixNano())
+	temp := fmt.Sprint(os.TempDir()+"/", rand.Float32(), ".avi")
+	aw, err := mjpeg.New(temp, 1920, 1080, 1)
+	if err != nil {
+		panic(err)
+	}
 
+	d := mp3.NewDecoder(f)
+
+	var (
+		frame   mp3.Frame
+		skipped = 0
+		dur     float64
+	)
+	for {
+		err := d.Decode(&frame, &skipped)
+		if err != nil {
+			break
+		}
+
+		dur = dur + frame.Duration().Seconds()
+
+	}
+
+	for i := 0; i < int(dur); i++ {
+		err = aw.AddFrame(m.Picture().Data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = aw.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command("ffmpeg", "-i", temp, "-i", in, "-codec", "copy", "-shortest", temp+".avi")
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	cmd2 := exec.Command("ffmpeg", "-i", temp+".avi", out)
+	if err := cmd2.Run(); err != nil {
+		panic(err)
+	}
 }
